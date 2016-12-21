eb71aa77d1da2964979522012bc1cb0cde612c32
xarly1602 <xarly1602@gmail.com>
2016-12-21 12:19:30 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b2b0#j^$fyu=+xp+aqw1&+=ftp+(=m-b1n-$fm3yzg^)dkyj#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
